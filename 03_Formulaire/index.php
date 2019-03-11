a5504e1a4544c142efca5e56b730e9577e28c7aa
Laugras <lew_terramp@hotmail.fr>
2019-03-11 15:50:54 +0100

    Ajout du Boutton et méthode post

--- 03_Formulaire/index.php
+++ 03_Formulaire/index.php
@@ -26,7 +26,7 @@
 
     <form class="container">
     <div class="row">
-        <div class="col-md-2 form-group">
+        <div class="col-md-2 form-group" action="worker.php" method="post">
         <label for="Praynom">Pray deul' nom</label>
             <input type="text" class="form-control" placeholder="Eul' Praynom">
         </div>
@@ -35,8 +35,8 @@
             <label for="exampleFormControlTextarea">Le message del la vida</label>
             <textarea class="form-control" id="exampleFormControlTextarea" rows="3"></textarea>
         </div>
+        <button type="submit" class="btn btn-primary">Envoyay</button>
     </form>
-
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
