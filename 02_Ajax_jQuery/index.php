b2d096b43762459f4b7b47f6d69e5e33086dff75
Laugras <lew_terramp@hotmail.fr>
2019-03-11 14:46:31 +0100

    Différence entre GET et POST. Par défault le type est GET, sauf quand on force avec le "type: POST"

--- 02_Ajax_jQuery/index.php
+++ 02_Ajax_jQuery/index.php
@@ -19,12 +19,21 @@
     <script>
 
 $(document).ready(function () {
-    // Exécuter une requête AJAX avec jQuery
+    // Exécuter une requête AJAX (en GET) avec jQuery
     $.get('../01-bases/worker.php').done(function (response) {
         alert(response);
     }).fail(function (xhr)){
         alert('La requête a échoué avec un status' +xhr.status);
     }
+
+    // Exécuter une requête AJAX (en POST)
+    $.ajax({
+        type: 'POST';
+        data: {sentence: 'Salut les gens'},
+        url: './worker.php'
+    }).done(function (response){
+        console.log(response);
+    })
 });
 
 
