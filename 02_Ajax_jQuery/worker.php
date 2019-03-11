fd23c531c1e2e550209b59adaecabe905d8d1712
Laugras <lew_terramp@hotmail.fr>
2019-03-11 14:55:20 +0100

    La *YODA condition*, pour mieux voire les conditions.

--- 02_Ajax_jQuery/worker.php
+++ 02_Ajax_jQuery/worker.php
@@ -1,4 +1,12 @@
 <?php
 
-print_r($_GET);
-print_r($_POST);
+// print_r($_GET);
+// print_r($_POST);
+
+if ('POST' === $_SERVER['REQUEST_METHOD']) 
+/* C'est une Yoda condition.
+Une affectation renvoie toujours true, 
+donc pour voir l'erreure, il inverse la condition */
+ {
+    echo $_POST['sentence'] ?? 'No sentence';
+}
