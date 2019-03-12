eb00055269c170b8309349383f1ff3cc2c8cb6f9
Laugras <lew_terramp@hotmail.fr>
2019-03-12 12:35:31 +0100

    Correction de la matinée du 12, Mannipulation des téléphones en bases de données.

--- 03_Formulaire/index.php
+++ 03_Formulaire/index.php
@@ -37,6 +37,11 @@
         </div>
         <button type="submit" class="btn btn-primary">Envoyay</button>
     </form>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script>
     var form = $('form');
 
@@ -53,15 +58,10 @@ form.on('submit', function (event) {
         // On peut forcer le contenu en JSON si le serveur
         // ne renvoie pas la bonne en-tête
         // dataType: 'json'
-    }).done(function (message) {
-        console.log(message);
+        }).done(function (message) {
+            console.log(message);
+        });
     });
-});
-
     </script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  
 </body>
 </html>
---
+++ 04_api/smartphone.php
@@ -0,0 +1,15 @@
+<?php
+
+// Connexion à la BDD
+$db = new PDO('mysql:host=mysql.docker;dbname=smartphone;charset=utf8', 'root', 'root', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+// On récupère les smartphones en associatif
+$sql = 'SELECT * FROM smartphone';
+$query = $db->query($sql);
+$smartphones = $query->fetchAll(PDO::FETCH_ASSOC);
+
+// Transformer le tableau en JSON (json_encode)
+header('Content-Type: application/json');
+echo json_encode($smartphones);
